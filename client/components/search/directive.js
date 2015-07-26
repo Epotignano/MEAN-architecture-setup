9e5ca2027256a9e8b16169c1b9e484ce7fce10f1
epotignano <epotignano@gmail.com>
2015-07-26 12:51:15 -0300

    Search box and suggestions list behaviour improved

--- client/components/search/directive.js
+++ client/components/search/directive.js
@@ -2,7 +2,11 @@
  * Created by emiliano on 23/07/15.
  */
 
-var searchBoxCtrl = ['suggestionsService', 'searchService', function(suggestionsService, searchService) {
+var searchBoxCtrl = [
+  'suggestionsService',
+  'searchService',
+  '$document',
+  function(suggestionsService, searchService, $document) {
 
   var search = this;
 
@@ -27,6 +31,12 @@ var searchBoxCtrl = ['suggestionsService', 'searchService', function(suggestions
 
   };
 
+  $document.bind('click', function(event) {
+    if( event.target.className.match('search-box')) {
+      search.getSuggestions();
+    }
+  })
+
   search.getResults = function() {
     searchService.search(search.terms)
       .then(function() {
@@ -34,21 +44,54 @@ var searchBoxCtrl = ['suggestionsService', 'searchService', function(suggestions
       })
   };
 
-  search.searchBySuggestion = function(suggestion) {
-    search.terms = suggestion;
-    search.getResults()
-      .then(function(results) {
+}];
+
+
+var suggestionsListCtrl = [
+  'suggestionsService',
+  'searchService',
+  '$element',
+  '$document',
+  function(suggestionsService, searchService, $element, $document) {
+
+    var suggestions = this;
+    suggestions.hide = false;
+
+    var elmScope = $element.scope();
+
+    suggestions.searchBySuggestion = function(suggestion) {
+      console.log(suggestion);
+
+      suggestions.selectedSuggestion = suggestion.key;
+    };
+
+    console.log($element);
+
+    $document.bind('click', function(event) {
+      if( (!event.target.className.match('results-list') || !event.target.className.match('search-box') ) && suggestions.list && suggestions.list.length) {
+        suggestions.list = [];
+        suggestions.hide = true;
+        elmScope.$apply();
+      }
+    })
+
+    $document.bind('click', function(event) {
+      if( !event.target.className.match('search-box')) {
+        suggestions.hide = false;
+        elmScope.$apply();
+      }
+    })
 
 
 
-      })
-  }
+
 
 }];
 
 
-angular.module('ui.search.box', ['nya.bootstrap.select'])
+angular.module('ui.search.box', [])
   .controller('searchBoxCtrl', searchBoxCtrl)
+  .controller('suggestionsListCtrl', suggestionsListCtrl)
    .directive('searchBox', ['searchService', function(searchService){
      // Runs during compile
      return {
@@ -62,6 +105,20 @@ angular.module('ui.search.box', ['nya.bootstrap.select'])
        templateUrl: 'components/search/template.html',
        bindToController : true
      };
-   }]);
+   }])
+
+  .directive('suggestionsList', ['searchService', function(){
+    // Runs during compile
+    return {
+      scope: {
+        list: '=',
+        selectedSuggestion: '='
+      }, // {} = isolate, true = child, false/undefined = no change
+      controller: "suggestionsListCtrl as suggestions",
+      restrict: 'EA', // E = Element, A = Attribute, C = Class, M = Comment
+      templateUrl: 'components/search/suggestions-list.html',
+      bindToController : true
+    };
+  }]);
 
 
